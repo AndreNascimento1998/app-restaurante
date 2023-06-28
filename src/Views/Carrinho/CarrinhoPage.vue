6ff614ab49039213b35ceb9fbc586adfb226c5f4
André Linux <andre.ncardoso@hotmail.com>
2023-06-28 19:29:29 -0300

    chore: carrinhpPage function less ok

--- src/Views/Carrinho/CarrinhoPage.vue
+++ src/Views/Carrinho/CarrinhoPage.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container v-if="itensCarrinho.length > 0">
-        <v-list v-for="item in itensCarrinho" :key="item.nome">
+        <v-list v-for="(item, index) in itensCarrinho" :key="item.nome">
             <v-card class="pa-0">
                 <v-row class="vertical-centro pa-2">
                     <v-col cols="1">
@@ -8,29 +8,42 @@
                             <img :src="item.src" width="100">
                         </v-avatar>
                     </v-col>
-                    <v-col cols="3">
+                    <v-col cols="2">
                         Item:  {{ item.nome }}
                     </v-col>
                     <v-col cols="2">
                         Valor:  R$ {{ (item.valor * item.qnt).toFixed(2) }}
                     </v-col>
                     <v-col cols="2">
-                        Quantidade:  {{ item.qnt }}
+                        text-qualquer
                     </v-col>
-                    <v-col cols="2" class="text-center">
-                            <DialogComponent 
-                                    :nome="item.nome" 
-                                    :src="item.src" 
-                                    :descricao="item.descricao"
-                                    :id="item.id"
-                                    :valor="item.valor"
-                            />
+                    <v-col cols="3" class="text-center">
+                        <v-btn 
+                            density="comfortable" @click="(retira(index, item))" 
+                            :disabled="item.qnt === 0"
+                        >
+                            <v-icon>mdi-minus</v-icon>
+                        </v-btn>
+
+                            Quantidade:  {{ item.qnt }}
+
+
+                        <v-btn density="comfortable" @click="(adiciona(index))">
+                            <v-icon>mdi-plus</v-icon>
+                        </v-btn>
+
+
                     </v-col>
+
+
                     <v-col cols="2" class="text-center">
-                        <v-btn @click="remover(item)" block variant="outlined" class="letra">
-                            Remover
+                        <v-btn 
+                        @click="remover(item)" block variant="outlined" class="letra"
+                    >
+                            Remover todos itens
                         </v-btn>
                     </v-col>
+
                 </v-row>
             </v-card>
         </v-list>
@@ -43,14 +56,12 @@
 <script setup>
 import { useCarrinhoCompras } from '@/stores/CarrinhoCompras'
 import { computed, onMounted } from 'vue'
-import DialogComponent from '@/components/CardapioComponents/DialogComponent.vue'
 
 const carrinhoStore = useCarrinhoCompras()
-
 const itensCarrinho = computed(() => carrinhoStore.carrinhoDeCompras)
 
+
 function remover(item){
-    console.log(item    )
     const indexItemExcluir = itensCarrinho.value.findIndex( (carrinho) => carrinho.id === item.id)
 
     if(indexItemExcluir !== -1){
@@ -58,6 +69,23 @@ function remover(item){
         carrinhoStore.itensQnt -= item.qnt
         carrinhoStore.carrinhoDeCompras.splice(indexItemExcluir, 1)
     }
+}
+
+function retira(index, item){
+    
+    carrinhoStore.carrinhoDeCompras[index].qnt -= 1
+    carrinhoStore.precoStore -= carrinhoStore.carrinhoDeCompras[index].valor
+    carrinhoStore.itensQnt -= 1
+    
+    if(item.qnt === 0){
+        remover(item)
+    }
+}
+
+function adiciona(index) {
+    carrinhoStore.carrinhoDeCompras[index].qnt += 1
+    carrinhoStore.precoStore += carrinhoStore.carrinhoDeCompras[index].valor
+    carrinhoStore.itensQnt += 1
 }
 
 onMounted(() => {
