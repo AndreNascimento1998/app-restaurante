fbcbbd9d7c2336ca938eb5dc12bedda49cc33981
André Linux <andre.ncardoso@hotmail.com>
2023-07-05 11:48:29 -0300

    feat: new rules for validation

---
+++ src/composable/useValidation.js
@@ -0,0 +1,22 @@
+
+const useValidation = () => {
+    const required = (value) => !!value || '*Campo obrigatório'
+    const emailMin = (value) => value.length > 6 || '*Maior que 6 dígitos'
+    const upperCase = (value) => /[A-Z]/.test(value) || '*Letra maiúscula AA'
+    const lowCase = (value) => /[a-z]/.test(value) || '*Letra minúscula aa'
+    const numberCase = (value) => /[1-9]/.test(value) || '*Número 1-9'
+    const minLenght = (value) => value.length > 8 || '*Maior que 8 dígitos'
+    const phoneMin = (value) => value.length > 13 || '*Maior que 13 dígitos'
+
+    return {
+        required,
+        emailMin,
+        upperCase,
+        lowCase,
+        numberCase,
+        minLenght,
+        phoneMin
+    }
+}
+
+export default useValidation
